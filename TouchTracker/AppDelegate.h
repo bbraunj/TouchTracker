489c5290b4f4721537d80513004a6486ac9d0c3e
Josh Braun <bbraunj@Joshs-MacBook-Pro.local>
2013-02-10 10:20:32 -0600

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Josh Braun on 2/10/13.
+//  Copyright (c) 2013 Josh Braun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Josh Braun on 2/10/13.
+//  Copyright (c) 2013 Josh Braun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
